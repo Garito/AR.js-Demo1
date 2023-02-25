af0ef4feef6b6ba3155ae137894a934ce1038b0a
Garito <garito@gmail.com>
2023-02-25 13:52:13 +0100

    Change text scale to the previous value

--- src/components/aframe-location.svelte
+++ src/components/aframe-location.svelte
@@ -20,6 +20,6 @@
   <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
   <!-- <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: { lat }; longitude: { lon }" scale="10 10 10" clicker></a-entity> -->
   <!-- <a-box gps-new-entity-place='latitude: { lat }; longitude: { lon }' color='red' scale='10 10 10' clicker></a-box> -->
-  <a-text gps-new-entity-place='latitude: { lat }; longitude: { lon }' look-at="[gps-new-camera]" scale="7 7 7" value="Click me!" align="center" clicker></a-text>
+  <a-text gps-new-entity-place='latitude: { lat }; longitude: { lon }' look-at="[gps-new-camera]" scale="5 5 5" value="Click me!" align="center" clicker></a-text>
 </a-scene>
 <div>Lon: { lon } Lat: { lat }</div>
