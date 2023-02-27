000ee4fcf008d8a9a36975c7439a58edf23588c7
Garito <garito@gmail.com>
2023-02-27 17:36:02 +0100

    More options

--- src/components/aframe-location.svelte
+++ src/components/aframe-location.svelte
@@ -3,6 +3,8 @@
 
   export let lat = 41.5378574;
   export let lon = 2.0434977;
+  export let entity = 'text';
+  export let value;
 
   onMount(() => {
     AFRAME.registerComponent('clicker', {
@@ -15,11 +17,13 @@
   })
 </script>
 
-<div>Lat: { lat } Lon: { lon }</div>
 <a-scene vr-mode-ui='enabled: false' cursor='rayOrigin: mouse' raycaster='near: 0; far: 50000' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: true' renderer='antialias: true; alpha: true'>
   <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
-  <!-- <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: { lat }; longitude: { lon }" scale="10 10 10" clicker></a-entity> -->
-  <!-- <a-box gps-new-entity-place='latitude: { lat }; longitude: { lon }' color='red' scale='10 10 10' clicker></a-box> -->
-  <a-text gps-new-entity-place='latitude: { lat }; longitude: { lon }' look-at="[gps-new-camera]" scale="5 5 5" value="Click me!" align="center" clicker></a-text>
+  {#if entity === 'text'}
+    <a-text gps-new-entity-place='latitude: { lat }; longitude: { lon }' look-at="[gps-new-camera]" scale="10 10 10" value={ value } align="center" clicker></a-text>
+  {:else if entity === 'cube'}
+    <a-box gps-new-entity-place='latitude: { lat }; longitude: { lon }' color={ value } scale='10 10 10' clicker></a-box>
+  {:else if entity === 'model'}
+    <a-entity gps-new-entity-place='latitude: { lat }; longitude: { lon }' scale="20 20 20" gltf-model="url({ value })" look-at="[gps-new-camera]" clicker></a-entity>
+  {/if}
 </a-scene>
-<div>Lon: { lon } Lat: { lat }</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,10 @@
 
   let lat;
   let lon;
+  let entity;
+  let value;
+
+  let go = false;
 
   const setLocation = () => {
     lat = $currentLocation.lat;
@@ -17,27 +21,40 @@
   onMount(() => { currentLocation.init() });
   // onDestroy(() => { currentLocation.close() });
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<div>
+<div class="p-5">
   <div class="current">
-    <h2>Location</h2>
-    <div>Latitude: { $currentLocation.lat }</div>
-    <div>Longitude: { $currentLocation.lon }</div>
-    <div>Accuracy: { $currentLocation.acc }</div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={ setLocation }>
+    <h2 class="text-xl">Location</h2>
+    <div>Latitude: { $currentLocation.lat } Longitude: { $currentLocation.lon } Accuracy: { $currentLocation.acc }</div>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4" on:click={ setLocation }>
       Set this location
     </button>
+    <div class="flex gap-4">
+      <input type="radio" id="text" name="entity" bind:group={ entity} value="text" />
+      <label for="text">Text</label>
+      <input type="radio" id="cube" name="entity" bind:group={ entity} value="cube" />
+      <label for="cube">Cube</label>
+      <input type="radio" id="model" name="entity" bind:group={ entity} value="model" />
+      <label for="model">Model</label>
+    </div>
   </div>
-  <div class="setted">
-    <h2>Setted</h2>
-    <div>Latitude: { lat }</div>
-    <div>Longitude: { lon }</div>
+  <div>
+    {#if ['text', 'model'].includes(entity) }
+      <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" bind:value={ value } />
+    {:else if entity === 'cube'}
+      <input type="color" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" bind:value={ value }>
+    {/if}
   </div>
+  <div class="mt-4">
+    <h2 class="text-xl">Setted</h2>
+    <div>Latitude: { lat } Longitude: { lon } Type: { entity } Value: { value }</div>
+  </div>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4" on:click={ () => { go = true } }>
+    Go!
+  </button>
 
   <!-- <ArLocation /> -->
-  {#if lat && lon}
-    <AframeLocation { lat } { lon } />
+  {#if go}
+    <AframeLocation { lat } { lon } { entity } { value } />
   {/if}
 </div>
